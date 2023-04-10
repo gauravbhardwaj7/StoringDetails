5ec5f80f977a1173a90400cff0759093ed5e0e50
gauravbhardwaj7 <106892217+gauravbhardwaj7@users.noreply.github.com>
2023-04-10 23:40:50 +0530

    Initial Commit

---
+++ StoringDetails/StoringDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoringDetailsApp.swift
+//  StoringDetails
+//
+//  Created by Gaurav Bhardwaj on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StoringDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
